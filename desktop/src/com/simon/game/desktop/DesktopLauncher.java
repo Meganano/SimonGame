1c5088bcf0b5370529396bafad80a05380acb52f
Kittiya Ku-kiattikun <iherep.hiw@hotmail.com>
2017-12-16 15:17:50 +0700

    Show Black window

---
+++ desktop/src/com/simon/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.simon.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.simon.game.SimonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 800;
+		new LwjglApplication(new SimonGame(), config);
+	}
+}
