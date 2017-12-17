116539b09989fe26e0e8de84fd58c2221d065288
Kittiya Ku-kiattikun <iherep.hiw@hotmail.com>
2017-12-17 13:05:48 +0700

    Detect key pressed

--- core/src/com/simon/game/GameScreen.java
+++ core/src/com/simon/game/GameScreen.java
@@ -5,11 +5,15 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class GameScreen extends ScreenAdapter {
 
-    public SimonGame simonGame;
+	public SimonGame simonGame;
     public static final Texture simonImg = new Texture("simon.jpg");
+    public static final Texture greenImg = new Texture("green.jpg");
+    public static final Texture redImg = new Texture("red.jpg");
+    public static final Texture yellowImg = new Texture("yellow.jpg");
+    public static final Texture blueImg = new Texture("blue.jpg");
 
     public GameScreen(SimonGame simonGame){
-        this.simonGame = simonGame;
+       this.simonGame = simonGame;
     }
     
     
--- core/src/com/simon/game/SimonGame.java
+++ core/src/com/simon/game/SimonGame.java
@@ -1,30 +1,68 @@
 package com.simon.game;
 
+import java.util.concurrent.TimeUnit;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SimonGame extends Game {
+public class SimonGame extends Game{
 	public SpriteBatch batch;
+	public int flash;
+
+	public void update() {
+		if (Gdx.input.isKeyJustPressed(Keys.A)) {
+			flash = 1;
+		}
+		if (Gdx.input.isKeyJustPressed(Keys.S)) {
+			flash = 2;
+		}
+		if (Gdx.input.isKeyJustPressed(Keys.Z)) {
+			flash = 3;
+		}
+		if (Gdx.input.isKeyJustPressed(Keys.X)) {
+			flash = 4;
+		}
+	}
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new GameScreen(this));
+		setScreen(new GameScreen(this)); 
 	}
 
 	@Override
 	public void render() {
-        batch.begin();
+		batch.begin();
+		update();
         batch.draw(GameScreen.simonImg, 0,0);
-        batch.end();
+        if(flash == 1) {
+        	System.out.println("GREEN");
+        	batch.draw(GameScreen.greenImg, 0,0);
+        }
+        if(flash == 2) {
+        	System.out.println("RED");
+        	batch.draw(GameScreen.redImg, 0,0);
+        }
+        if(flash == 3) {
+        	System.out.println("YELLOW");
+        	batch.draw(GameScreen.yellowImg, 0,0);
+        }
+        if(flash == 4) {
+        	System.out.println("BLUE");
+        	batch.draw(GameScreen.blueImg, 0,0);
+        }
+        
+       flash = 0;
+       batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-	}
+	}	
 }
